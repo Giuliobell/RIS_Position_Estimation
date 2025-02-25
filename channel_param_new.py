fa75f4fc46ad15bac88fac7067c767d646e41eef
GiulioBellese <giuly.bellese@gmail.com>
2025-02-25 12:44:46 +0100

    estimated inner angle (large error on estimation)

--- channel_param_new.py
+++ channel_param_new.py
@@ -6,6 +6,7 @@ L = 1     #Numero di percorsi
 N = 16    #Numero di antenne RIS
 M = 4     #Numero di antenne BS
 K = 4     #Numero di antenne UE
+D = 15    #Numero di configurazioni di RIS
 
 Theta_F = 1.1071
 Theta_G = 0.9273
@@ -53,7 +54,38 @@ def initialize_noise_Matrix(row, column):
     
     return matr
 
+def dft_matrix(N):
+    """Crea una matrice DFT di dimensione NxN."""
+    n = np.arange(N)
+    k = n.reshape((N, 1))
+    omega = (1/np.sqrt(N)) * np.exp(-2j * np.pi * k * n / N)
+    return omega
 
+def initialize_K_Matrix(D,N, Theta):
+    matr = np.zeros((N,D), dtype=np.complex128)
+    
+    matr[0:D, 0:D] = Theta
+    
+    return matr
+
+def estract_angles(cos_estim):
+    # Assumendo che gli angoli di RIS siano compresi nell'intevallo  [30°,150°]
+    start_angle = np.pi / 6
+    end_angle = 5 * np.pi / 6
+    
+    best_phi_G = None
+    best_Theta_F = None
+    min_diff = float('inf')
+    
+    for phi_G in np.linspace(start_angle, end_angle, 5000):
+        for Theta_F in np.linspace(start_angle, end_angle, 5000):
+            diff = abs(np.cos(phi_G) - np.cos(Theta_F) - cos_estim)
+            if diff < min_diff:
+                min_diff = diff
+                best_phi_G = phi_G
+                best_Theta_F = Theta_F
+                
+    return best_phi_G, best_Theta_F
 
 #****** PARTE PRINCIPALE DEL CODICE ******
 #++++ Modello di Sistema ++++
@@ -79,7 +111,6 @@ H = G @ Omega @ F
 #++++ Stima dei Parametri del Canale ++++
 #Calcolo Angoli Esterni
 #Theta_G
-X = initialize_pilotVector_matrix(K, K)
 N_noise = initialize_noise_Matrix(M,K)
 V = H + N_noise 
 
@@ -93,14 +124,41 @@ R_HH = V.conj().T @ V
 estim_2, pow = eng.rootmusic(R_HH, 1, 'corr', nargout=2)
 phi_F_hat = np.arccos(-estim_2/np.pi)
 
-eng.quit()
 
 #Precoder e Combiner
 P = initialize_A_N_matrix(phi_F_hat, K, L)
-Q = initialize_A_N_matrix(Theta_G_hat, M, L)
+W = initialize_A_N_matrix(Theta_G_hat, M, L)
 
 #Calcolo Angoli Interni
+Theta = dft_matrix(D)
+K = initialize_K_Matrix(D,N, Theta)
+
+Gamma = np.kron(Gamma_F,Gamma_G)
+A_N_psi = khatri_rao_prod(A_N_Theta_F.conj().T, A_N_phi_G.T)
+A_N_psi = A_N_psi.T
+
+Z = np.kron(P.T@A_K_phi_F.conj(), (W.conj().T@A_M_Theta_G)@Gamma@A_N_psi.conj().T)
+
+Y = Z@K #+N
+
+Z_hat = (1/D) * Y @ Theta.conj().T
+R_ZZ = Z_hat.conj().T@Z_hat
+
+#Stima angoli interni
+estim_3, pow = eng.rootmusic(R_ZZ, 1, 'corr', nargout=2)
+
+phi_G, Theta_F = estract_angles(np.cos(abs(estim_3)))
+
+if phi_G is not None and Theta_F is not None:
+    print(f"phi_G: {phi_G}, Theta_F: {Theta_F}")
+else:
+    print("Non è stato possibile trovare una combinazione di angoli che soddisfi l'equazione.")
+
 
 
 print("Theta_G_hat: ", Theta_G_hat)
 print("phi_F_hat: ", phi_F_hat)
+eng.quit()
+
+
+#Vettore angoli interni = 1.210294875
